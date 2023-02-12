c75073e28ab40f5c997b19e8c3bb7fa9b4d6f10d
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-12 17:23:34 +0530

    Event for "Escape" button

--- script.js
+++ script.js
@@ -21,3 +21,11 @@ btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
+
+
+document.addEventListener('keydown', function(e){
+    
+    if(e.key === 'Escape' && !modal.classList.contains('hidden'))
+        closeModal();
+    
+})
